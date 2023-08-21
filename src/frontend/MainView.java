67389eee9ef39fa041741a07455d96b5802219d2
Diego Rocha <drochaesc@gmail.com>
2023-08-20 19:41:56 -0600

    Add Dining Room Design

--- src/frontend/MainView.java
+++ src/frontend/MainView.java
@@ -26,18 +26,22 @@ public class MainView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panel_bg = new javax.swing.JPanel();
         panel_footer = new javax.swing.JPanel();
         img_philosopher1 = new javax.swing.JLabel();
         img_philosopher2 = new javax.swing.JLabel();
         img_philosopher3 = new javax.swing.JLabel();
         img_philosopher4 = new javax.swing.JLabel();
         img_philosopher5 = new javax.swing.JLabel();
+        lbl_numPhilosopher1 = new javax.swing.JLabel();
+        lbl_numPhilosopher2 = new javax.swing.JLabel();
+        lbl_numPhilosopher3 = new javax.swing.JLabel();
+        lbl_numPhilosopher4 = new javax.swing.JLabel();
+        lbl_numPhilosopher5 = new javax.swing.JLabel();
+        img_background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
-
-        panel_bg.setBackground(new java.awt.Color(102, 0, 0));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         panel_footer.setBackground(new java.awt.Color(0, 0, 0));
 
@@ -45,81 +49,53 @@ public class MainView extends javax.swing.JFrame {
         panel_footer.setLayout(panel_footerLayout);
         panel_footerLayout.setHorizontalGroup(
             panel_footerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 594, Short.MAX_VALUE)
+            .addGap(0, 320, Short.MAX_VALUE)
         );
         panel_footerLayout.setVerticalGroup(
             panel_footerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 179, Short.MAX_VALUE)
+            .addGap(0, 600, Short.MAX_VALUE)
         );
+
+        getContentPane().add(panel_footer, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 320, 600));
 
         img_philosopher1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/eating.png"))); // NOI18N
+        getContentPane().add(img_philosopher1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 90, -1, -1));
 
         img_philosopher2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/thinking.png"))); // NOI18N
+        getContentPane().add(img_philosopher2, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 110, -1, -1));
 
         img_philosopher3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/waiting.png"))); // NOI18N
+        getContentPane().add(img_philosopher3, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 300, -1, -1));
 
         img_philosopher4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/sleeping.png"))); // NOI18N
+        getContentPane().add(img_philosopher4, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 230, -1, -1));
 
         img_philosopher5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/thinking.png"))); // NOI18N
+        getContentPane().add(img_philosopher5, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 330, -1, -1));
 
-        javax.swing.GroupLayout panel_bgLayout = new javax.swing.GroupLayout(panel_bg);
-        panel_bg.setLayout(panel_bgLayout);
-        panel_bgLayout.setHorizontalGroup(
-            panel_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panel_footer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(panel_bgLayout.createSequentialGroup()
-                .addGap(237, 237, 237)
-                .addComponent(img_philosopher1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel_bgLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(img_philosopher3)
-                .addGap(160, 160, 160))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel_bgLayout.createSequentialGroup()
-                .addGap(95, 95, 95)
-                .addComponent(img_philosopher4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(img_philosopher2)
-                .addGap(93, 93, 93))
-            .addGroup(panel_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(panel_bgLayout.createSequentialGroup()
-                    .addGap(165, 165, 165)
-                    .addComponent(img_philosopher5)
-                    .addContainerGap(329, Short.MAX_VALUE)))
-        );
-        panel_bgLayout.setVerticalGroup(
-            panel_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel_bgLayout.createSequentialGroup()
-                .addGap(16, 16, 16)
-                .addComponent(img_philosopher1)
-                .addGroup(panel_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(panel_bgLayout.createSequentialGroup()
-                        .addGap(4, 4, 4)
-                        .addComponent(img_philosopher2))
-                    .addGroup(panel_bgLayout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(img_philosopher4)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(img_philosopher3)
-                .addGap(33, 33, 33)
-                .addComponent(panel_footer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(panel_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel_bgLayout.createSequentialGroup()
-                    .addContainerGap(275, Short.MAX_VALUE)
-                    .addComponent(img_philosopher5)
-                    .addGap(202, 202, 202)))
-        );
+        lbl_numPhilosopher1.setFont(new java.awt.Font("Franklin Gothic Demi Cond", 0, 48)); // NOI18N
+        lbl_numPhilosopher1.setText("1");
+        getContentPane().add(lbl_numPhilosopher1, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 170, -1, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panel_bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panel_bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        lbl_numPhilosopher2.setFont(new java.awt.Font("Franklin Gothic Demi Cond", 0, 48)); // NOI18N
+        lbl_numPhilosopher2.setText("2");
+        getContentPane().add(lbl_numPhilosopher2, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 180, -1, -1));
+
+        lbl_numPhilosopher3.setFont(new java.awt.Font("Franklin Gothic Demi Cond", 0, 48)); // NOI18N
+        lbl_numPhilosopher3.setText("3");
+        getContentPane().add(lbl_numPhilosopher3, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 310, -1, -1));
+
+        lbl_numPhilosopher4.setFont(new java.awt.Font("Franklin Gothic Demi Cond", 0, 48)); // NOI18N
+        lbl_numPhilosopher4.setText("4");
+        getContentPane().add(lbl_numPhilosopher4, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 320, -1, -1));
+
+        lbl_numPhilosopher5.setFont(new java.awt.Font("Franklin Gothic Demi Cond", 0, 48)); // NOI18N
+        lbl_numPhilosopher5.setText("5");
+        getContentPane().add(lbl_numPhilosopher5, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 250, -1, -1));
+
+        img_background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Imagen de WhatsApp 2023-08-20 a las 19.32.28 (1).jpg"))); // NOI18N
+        img_background.setText("jLabel1");
+        getContentPane().add(img_background, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 0, 620, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -160,12 +136,17 @@ public class MainView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel img_background;
     private javax.swing.JLabel img_philosopher1;
     private javax.swing.JLabel img_philosopher2;
     private javax.swing.JLabel img_philosopher3;
     private javax.swing.JLabel img_philosopher4;
     private javax.swing.JLabel img_philosopher5;
-    private javax.swing.JPanel panel_bg;
+    private javax.swing.JLabel lbl_numPhilosopher1;
+    private javax.swing.JLabel lbl_numPhilosopher2;
+    private javax.swing.JLabel lbl_numPhilosopher3;
+    private javax.swing.JLabel lbl_numPhilosopher4;
+    private javax.swing.JLabel lbl_numPhilosopher5;
     private javax.swing.JPanel panel_footer;
     // End of variables declaration//GEN-END:variables
 }
